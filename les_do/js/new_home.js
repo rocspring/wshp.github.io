93478b1a027152ce99ea914ccfa28218fcb9ca67
shaopengwang <shaopengwang@sohu-inc.com>
2015-02-05 18:33:12 +0800

    修改小屏手机

--- les_do/js/new_home.js
+++ les_do/js/new_home.js
@@ -526,7 +526,7 @@
 
 		//对3.5寸的iphone4,iphone4s手机背景图进行特殊处理
 		if (window.screen.height === 480 ) {
-			$('.ask .container .bg').css('background-position', '0 80px');
+			$('.ask .container .bg').css('background-position', '0 40px');
 		}
 
 		$answer.show();
